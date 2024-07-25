c8a2f15f62f625cf2a2c997cf71793e29d3b1879
Sumit Dubey <149804568+Sumit0ubey@users.noreply.github.com>
2024-07-25 20:58:55 +0530

    Add files via upload

---
+++ Data Analysis/Task_2.py
@@ -0,0 +1,29 @@
+"""
+TASK - 2: Data Manipulation with Pandas
+"""
+
+import pandas as pd
+
+data = pd.read_csv('S:/VS code/Internship/Data Analysis internship/01.Data Cleaning and Preprocessing.csv')
+
+# Data Summary
+data.info()
+print(data.head())
+
+
+# Handling missing data
+updated_data = data.fillna(data.mean(numeric_only=True))
+print(updated_data.head(10))
+
+#Summary statistics
+print(data.describe)
+
+print("Performing stats on Column BlowFlow")
+print("Mean: ", round(data['BlowFlow'].mean(), 3))
+print("Median: ", round(data['BlowFlow'].median(), 3))
+print("MAX value: ", round(data['BlowFlow'].max(), 3))
+print("MIN Value: ", round(data['BlowFlow'].min(), 3))
+
+q1, q2, q3 = round(data['BlowFlow'].quantile(0.25), 2), round(data['BlowFlow'].quantile(0.50), 2), round(data['BlowFlow'].quantile(0.75), 2)
+IQR = round((q3 - q1), 3)
+print("Inter quartile range on Column BlowFlow: ", IQR)
