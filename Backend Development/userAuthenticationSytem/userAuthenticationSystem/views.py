28968feee22d209c3be4c1a7039bb7c54c507302
Sumit Dubey <149804568+Sumit0ubey@users.noreply.github.com>
2024-09-09 13:36:24 +0530

    Create views.py

---
+++ Backend Development/userAuthenticationSytem/userAuthenticationSystem/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.contrib.auth import authenticate
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import AllowAny
+
+from users.serializers import UserSerializers
+
+class RegisterUser(APIView):
+    def post(self, request):
+        serializer = UserSerializers(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "User created successfully"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LoginUser(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(username=username, password=password)
+
+        if user:
+            token, _ = Token.objects.get_or_create(user=user)
+            return Response({"token": token.key}, status=status.HTTP_200_OK)
+        return Response({"error": "Invalid credentials"}, status=status.HTTP_400_BAD_REQUEST)
+
