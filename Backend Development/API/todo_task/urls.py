f314aae166813726fc759e7b45f0a2ae04eb06c8
Sumit Dubey <149804568+Sumit0ubey@users.noreply.github.com>
2024-09-12 09:27:27 +0530

    Update urls.py

--- Backend Development/API/todo_task/urls.py
+++ Backend Development/API/todo_task/urls.py
@@ -21,8 +21,6 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('todo.urls')),
-    path('user/', include('users.urls')),
     path('api/', include('api.urls')),
     path('auth/', include('rest_framework.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
